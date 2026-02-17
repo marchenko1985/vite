7b7c9edc709f9eeba6be9d1a7e38d4d92afcc3c4
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2026-02-17 09:04:48 +0200

    upgrd

--- App.tsx
+++ App.tsx
@@ -1,11 +1,5 @@
+import { ComponentExample } from "./components/component-example";
+
 export function App() {
-  return (
-    <div>
-      <h1>Hello Vite</h1>
-      <div className="flex gap-4">
-        <input type="text" />
-        <button className="btn">hello</button>
-      </div>
-    </div>
-  );
+  return <ComponentExample />
 }
